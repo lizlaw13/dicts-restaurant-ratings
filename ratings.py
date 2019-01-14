f45e0f5632f332de35fe582dbf645b7dd1a51ffc
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-14 12:52:47 -0800

    add user prompt on restaurant and ratings

--- ratings.py
+++ ratings.py
@@ -7,9 +7,16 @@ def restaurant_ratings(filename):
         for rating in restaurant_info:
             ratings[restaurant_info[0]] = restaurant_info[1]
 
+    new_restaurant = input('Please add a new restaurant:')
+    new_rating = input('Please add a rating for the restaurant:')
+
+    ratings[new_restaurant] = new_rating
+
     sorted_restaurant_ratings = sorted(ratings.items())
     for restaurant, restaurant_ratings in sorted_restaurant_ratings:
         print(f'{restaurant} is rated at {restaurant_ratings}.')
+
+
 
 
 restaurant_ratings('scores.txt')
