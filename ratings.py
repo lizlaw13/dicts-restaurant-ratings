897f214603b54242be9372080aaa24ebc02642d4
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-14 13:02:23 -0800

    validate new score added

--- ratings.py
+++ ratings.py
@@ -6,16 +6,21 @@ def restaurant_ratings(filename):
         restaurant_info = line.split(':')
         for rating in restaurant_info:
             ratings[restaurant_info[0]] = restaurant_info[1]
-
+           
     new_restaurant = input('Please add a new restaurant:')
     new_rating = input('Please add a rating for the restaurant:')
+    if int(new_rating) <= 0 or int(new_rating) >= 5:
+        print('Please provide a valid number for ratings(1-5)')
+        new_rating = input('Please add a rating for the restaurant again:')
 
-    ratings[new_restaurant] = new_rating
 
-    sorted_restaurant_ratings = sorted(ratings.items())
-    for restaurant, restaurant_ratings in sorted_restaurant_ratings:
-        print(f'{restaurant} is rated at {restaurant_ratings}.')
 
+    else:
+        ratings[new_restaurant] = new_rating
+
+        sorted_restaurant_ratings = sorted(ratings.items())
+        for restaurant, restaurant_ratings in sorted_restaurant_ratings:
+            print(f'{restaurant} is rated at {restaurant_ratings}.')
 
 
 
